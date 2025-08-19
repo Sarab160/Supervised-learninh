a43583b11293280bfeb7542179c0c34800daa871
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2025-08-19 02:14:00 -0700

    SVR

--- SVR/svr.py
+++ SVR/svr.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV,RandomizedSearchCV
 from sklearn.svm import SVR
 
+
 df=pd.read_csv(r"E:\python\supervised learning\SVR\rainfall.csv")
+print(df.columns)
+# print(df[" area"].nunique())
+
 df["average_rain_fall_mm_per_year"] = pd.to_numeric(df["average_rain_fall_mm_per_year"], errors="coerce")
 
 
@@ -12,14 +16,31 @@ df["average_rain_fall_mm_per_year"]=df["average_rain_fall_mm_per_year"].fillna(d
 print(df.info())
 
 le=LabelEncoder()
-df["Area"]=le.fit_transform(df["Area"])
-x=df[["Area","Year"]]
+df[" area"]=le.fit_transform(df[" area"])
+x=df[[" area","Year"]]
 y=df["average_rain_fall_mm_per_year"]
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 
-svr=SVR(kernel="linear")
+svr=SVR(kernel="poly")
 svr.fit(x_train,y_train)
 
 print(svr.score(x_test,y_test))
+
+param_grid = {
+    "kernel": ["linear", "rbf", "poly", "sigmoid"],
+    "C": [0.1, 1, 10, 100],
+    "gamma": ["scale", "auto", 0.01, 0.1, 1],
+    "epsilon": [0.01, 0.1, 0.5, 1],
+    "degree": [2, 3, 4]  # used only if kernel='poly'
+}
+
+gd=RandomizedSearchCV(estimator=SVR(),param_distributions=param_grid,cv=5,n_iter=10)
+gd.fit(x_train,y_train)
+
+print(gd.best_params_)
+print(gd.best_score_)
+
+
+
 
